289636a44df03d9577814cd0bf66b311829d6ad9
MxolisiSibaya322 <76241173+MxolisiSibaya322@users.noreply.github.com>
2021-10-17 14:49:16 +0200

    Files

---
+++ script.js
@@ -0,0 +1,41 @@
+function revealMusic() {
+  let page = document.getElementById("musicTab");
+  let button = document.getElementById("musicPlay");
+  let counter = true;
+
+  page.style.display = "block";
+
+  button.onclick = function () {
+     if(counter){
+    page.style.display = "block";
+    counter= false
+   }else{
+    page.style.display = "none";
+    counter= true;
+   }
+  };
+
+  let mySong = document.getElementById("mySongs");
+  let icon = document.getElementById("play");
+  let my2Song = document.getElementById("mySong");
+  let icon2 = document.getElementById("play2");
+  icon.onclick = function () {
+    if (mySong.paused) {
+      mySong.play();
+      icon.src = "media/Pause.png";
+    } else {
+      mySong.pause();
+      icon.src = "media/play.png";
+    }
+  };
+
+  icon2.onclick = function () {
+    if (my2Song.paused) {
+      my2Song.play();
+      icon2.src = "media/Pause.png";
+    } else {
+      my2Song.pause();
+      icon2.src = "media/play.png";
+    }
+  };
+}
