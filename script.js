ae710caaaa3584b2ad0eb9c68ae20d2efecd45a1
Mxolisi <mxolisi.gojolo@gmail.com>
2022-01-12 08:32:18 +0200

    website push

--- script.js
+++ script.js
@@ -16,30 +16,46 @@ function revealMusic() {
   //   counter= true;
   //  }
   // };
+}
 
+function playMusic() {
   let mySong = document.getElementById("mySongs");
-  let icon = document.getElementById("play");
+  let stop1 = document.getElementById("stop1");
+  let stop2 = document.getElementById("stop2");
+  let icon = document.getElementById("audio1");
   let my2Song = document.getElementById("mySong");
-  let icon2 = document.getElementById("play2");
+  let icon2 = document.getElementById("audio2");
   icon.onclick = function () {
     if (mySong.paused) {
       mySong.play();
-      icon.src = "media/Pause.png";
+      icon.innerHTML = "Pause &#9208";
     } else {
       mySong.pause();
-      icon.src = "media/play.png";
+      icon.innerHTML = "play &#9658";
     }
   };
-
+  stop1.onclick = function () {
+    mySong.pause();
+    mySong.currentTime = 0;
+    icon.innerHTML = "play &#9658";
+  };
+  stop2.onclick = function () {
+    my2Song.pause();
+    my2Song.currentTime = 0;
+    icon2.innerHTML = "play &#9658";
+  };
   icon2.onclick = function () {
     if (my2Song.paused) {
       my2Song.play();
-      icon2.src = "media/Pause.png";
+      icon2.innerHTML = "Pause &#9208";
     } else {
       my2Song.pause();
-      icon2.src = "media/play.png";
+      icon2.innerHTML = "play &#9658";
     }
   };
+}
+function stop() {
+  playMusic();
 }
 function openInNewTab(){
   window.open("media/MxolisiCV.pdf", '_blank').focus();
